e448b53b421bee9d9d0e9aa8416087b684f4c255
mehwish 309 <mehwish309@gmail.com>
2020-09-03 13:03:39 +0100

    First Project

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  //state stuff
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  //use Effect
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+  //Functions
+  const filterHandler = () => {
+    switch (status) {
+      case "Completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+  return (
+    <div className="App">
+      <header>
+        <h1>Mehwish's Todo List </h1> 
+      </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        todos={todos}
+        setTodos={setTodos}
+      />
+    </div>
+  );
+}
+
+export default App;
